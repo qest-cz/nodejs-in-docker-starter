22417ee01f1ac02c66af911b18e61be2bec9aa8b
Cyril Urban <cyril.urban@qest.cz>
2023-06-04 21:31:30 +0200

    feat(init-commit): add runable node project via docker-compose up

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+// open project dev-image in your IDE and open terminal in this directory
+// $ docker build -t node-dev-image .
+// $ docker run -it --rm --name dev -v $(pwd):/code node-dev-image bash
+// $ cd code
+// $ yarn
+// $ yarn dev
+
+// dat to do docker-compose a v ramci dockerfile zutomatizovat cd code && yarn && yarn dev
+
+console.log('Qest is best')
